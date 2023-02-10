4908ea2ac5528ecfb8fa604558b66ff99dca579c
Mike Dilger <mike@mikedilger.com>
2023-02-10 13:38:30 +1300

    Setup for a personal relay

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,12 @@
 use nostr_caesar::nostr_caesar_client::NostrCaesarClient;
-use nostr_caesar::{Event, Filter};
+use nostr_caesar::{RpcEventCheck, RpcFilterCheck};
+use nostr_types::{Event, EventKind, Filter, PreEvent, PrivateKey, PublicKey, PublicKeyHex, Tag, Unixtime};
+
+const EXAMPLE_OWNER_PUBKEY: &'static str = "00000b6b73abad367cd1924ab6700d381582a585ddc40c25df2b2be6d737488f";
+const EXAMPLE_OWNER_PRVKEY: &'static str = "93342b31aee9d0597bdf9d851ab8ea6b724c83752dac8de4e4c90bee0054ebec";
+
+const EXAMPLE_OTHER_PUBKEY: &'static str = "f332a54f2bd94988dce7f56d3df5845c2a301455c3bdd387b5c2ff0f8ca1c55a";
+const EXAMPLE_OTHER_PRVKEY: &'static str = "cbfa9fb8b54ec27e570da7e27a454aec5f8550f61dcc71d7a932967b3d53455c";
 
 pub mod nostr_caesar {
     tonic::include_proto!("nostr_caesar");
@@ -9,17 +16,131 @@ pub mod nostr_caesar {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = NostrCaesarClient::connect("http://127.0.0.20:50051").await?;
 
-    let request = tonic::Request::new(Event {
-        serialized: "SERIALIZED_EVENT".into(),
+    let owner_private_key = PrivateKey::try_from_hex_string(EXAMPLE_OWNER_PRVKEY)?;
+    let other_private_key = PrivateKey::try_from_hex_string(EXAMPLE_OTHER_PRVKEY)?;
+    let owner_pubkeyhex = PublicKeyHex::try_from_str(EXAMPLE_OWNER_PUBKEY)?;
+    let other_pubkeyhex = PublicKeyHex::try_from_str(EXAMPLE_OTHER_PUBKEY)?;
+
+    // ---------------------------------------------------------
+    // Test the author's events pass
+    let pre_event = PreEvent {
+        pubkey: PublicKey::try_from_hex_string(EXAMPLE_OWNER_PUBKEY)?,
+        created_at: Unixtime::now()?,
+        kind: EventKind::TextNote,
+        tags: vec![],
+        content: "This is a top level post going into my outbox".to_owned(),
+        ots: None,
+    };
+    let event = Event::new(pre_event, &owner_private_key)?;
+    let request = tonic::Request::new(RpcEventCheck {
+        serialized: serde_json::to_string(&event)?,
     });
     let response = client.allow_post(request).await?;
-    println!("RESPONSE={:?}", response);
+    assert!( response.get_ref().answer );
 
-    let request = tonic::Request::new(Filter {
-        serialized: "SERIALIZED_FILTER".into(),
+    // ---------------------------------------------------------
+    // Test someone else's events fail
+    let pre_event = PreEvent {
+        pubkey: PublicKey::try_from_hex_string(EXAMPLE_OTHER_PUBKEY)?,
+        created_at: Unixtime::now()?,
+        kind: EventKind::TextNote,
+        tags: vec![],
+        content: "This is someone who is not authorized to post here".to_owned(),
+        ots: None,
+    };
+    let event = Event::new(pre_event, &other_private_key)?;
+    let request = tonic::Request::new(RpcEventCheck {
+        serialized: serde_json::to_string(&event)?,
+    });
+    let response = client.allow_post(request).await?;
+    assert!( !response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test someone elses kind-10002 events pass
+    let pre_event = PreEvent {
+        pubkey: PublicKey::try_from_hex_string(EXAMPLE_OTHER_PUBKEY)?,
+        created_at: Unixtime::now()?,
+        kind: EventKind::RelayList,
+        tags: vec![], // no relays, this doesn't really matter
+        content: "".to_owned(),
+        ots: None,
+    };
+    let event = Event::new(pre_event, &other_private_key)?;
+    let request = tonic::Request::new(RpcEventCheck {
+        serialized: serde_json::to_string(&event)?,
+    });
+    let response = client.allow_post(request).await?;
+    assert!( response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test someone elses events tagging me pass
+    let pre_event = PreEvent {
+        pubkey: PublicKey::try_from_hex_string(EXAMPLE_OTHER_PUBKEY)?,
+        created_at: Unixtime::now()?,
+        kind: EventKind::RelayList,
+        tags: vec![Tag::Pubkey { pubkey: owner_pubkeyhex.clone(),
+                                 recommended_relay_url: None,
+                                 petname: None } ],
+        content: "Tag, you're it!".to_owned(),
+        ots: None,
+    };
+    let event = Event::new(pre_event, &other_private_key)?;
+    let request = tonic::Request::new(RpcEventCheck {
+        serialized: serde_json::to_string(&event)?,
+    });
+    let response = client.allow_post(request).await?;
+    assert!( response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test kind 10002 Filters pass
+    let filter = Filter {
+        kinds: vec![EventKind::RelayList],
+        .. Default::default()
+    };
+    let request = tonic::Request::new(RpcFilterCheck {
+        serialized: serde_json::to_string(&filter)?,
     });
     let response = client.allow_filter(request).await?;
-    println!("RESPONSE={:?}", response);
+    assert!( response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test owner-author Filters pass
+    let filter = Filter {
+        authors: vec![owner_pubkeyhex.prefix(20)],
+        .. Default::default()
+    };
+    let request = tonic::Request::new(RpcFilterCheck {
+        serialized: serde_json::to_string(&filter)?,
+    });
+    let response = client.allow_filter(request).await?;
+    assert!( response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test other-kind Filters fail
+    let filter = Filter {
+        kinds: vec![EventKind::Reaction],
+        .. Default::default()
+    };
+    let request = tonic::Request::new(RpcFilterCheck {
+        serialized: serde_json::to_string(&filter)?,
+    });
+    let response = client.allow_filter(request).await?;
+    assert!( ! response.get_ref().answer );
+
+    // ---------------------------------------------------------
+    // Test other-author Filters fail
+    let filter = Filter {
+        authors: vec![other_pubkeyhex.prefix(20)],
+        .. Default::default()
+    };
+    let request = tonic::Request::new(RpcFilterCheck {
+        serialized: serde_json::to_string(&filter)?,
+    });
+    let response = client.allow_filter(request).await?;
+    assert!( ! response.get_ref().answer );
+
+    println!("All tests passed.");
 
     Ok(())
+
 }
--- src/server.rs
+++ src/server.rs
@@ -1,42 +1,95 @@
+use nostr_caesar::nostr_caesar_server::{NostrCaesar, NostrCaesarServer};
+use nostr_caesar::{RpcEventCheck, RpcFilterCheck, RpcAnswer};
+use nostr_types::{Event, EventKind, Tag, Filter, PublicKeyHex};
 use tonic::{transport::Server, Request, Response, Status};
 
-use nostr_caesar::nostr_caesar_server::{NostrCaesar, NostrCaesarServer};
-use nostr_caesar::{Event, Filter, Answer};
+const EXAMPLE_PUBKEY: &'static str = "00000b6b73abad367cd1924ab6700d381582a585ddc40c25df2b2be6d737488f";
 
 pub mod nostr_caesar {
     tonic::include_proto!("nostr_caesar");
 }
 
-#[derive(Debug, Default)]
-pub struct MyNostrCaesar {}
+#[derive(Debug)]
+pub struct MyNostrCaesar {
+    owner_pubkey: PublicKeyHex,
+}
 
 #[tonic::async_trait]
 impl NostrCaesar for MyNostrCaesar {
     async fn allow_post(
         &self,
-        request: Request<Event>,
-    ) -> Result<Response<Answer>, Status> {
-        println!("Denying all requests. request from {:?}", request.remote_addr());
-        Ok(Response::new(Answer { answer: false }))
+        request: Request<RpcEventCheck>,
+    ) -> Result<Response<RpcAnswer>, Status> {
+
+        let event: Event = serde_json::from_str(&request.get_ref().serialized)
+            .map_err(|e| Status::invalid_argument(format!("{}", e)))?;
+
+        // Allow if I am AUTHed (so I have full access)
+        // TBD
+
+        // Allow kind-10002 for everybody
+        if event.kind == EventKind::RelayList {
+            return Ok(Response::new(RpcAnswer { answer: true }));
+        }
+
+        // Allow if I am the author (so I can push into my outbox)
+        let event_pubkey: PublicKeyHex = event.pubkey.into();
+        if event_pubkey == self.owner_pubkey {
+            return Ok(Response::new(RpcAnswer { answer: true }));
+        }
+
+        // Allow if I am tagged (so the world can push into my inbox)
+        if event.tags.iter().any(|t| {
+            if let Tag::Pubkey { pubkey, .. } = t {
+                *pubkey == self.owner_pubkey
+            } else {
+                false
+            }
+        }) {
+            return Ok(Response::new(RpcAnswer { answer: true }));
+        }
+
+        println!("Denying EVENT: {}", &request.get_ref().serialized);
+        Ok(Response::new(RpcAnswer { answer: false }))
     }
 
     async fn allow_filter(
         &self,
-        request: Request<Filter>,
-    ) -> Result<Response<Answer>, Status> {
-        println!("Denying all filters. request from {:?}", request.remote_addr());
-        Ok(Response::new(Answer { answer: false }))
+        request: Request<RpcFilterCheck>,
+    ) -> Result<Response<RpcAnswer>, Status> {
+
+        let filter: Filter = serde_json::from_str(&request.get_ref().serialized)
+            .map_err(|e| Status::invalid_argument(format!("{}", e)))?;
+
+        // Allow if I am AUTHed (so I have full access)
+        // TBD
+
+        // Allow if strictly kind-10002 for everybody
+        if filter.kinds == vec![EventKind::RelayList] {
+            return Ok(Response::new(RpcAnswer { answer: true }));
+        }
+
+        // Allow if I am the only author in the filter (so the world can pull from my outbox)
+        if filter.authors.len() == 1 && filter.authors[0].matches(&self.owner_pubkey) {
+            return Ok(Response::new(RpcAnswer { answer: true }));
+        }
+
+        println!("Denying REQ Filter: {}", &request.get_ref().serialized);
+        Ok(Response::new(RpcAnswer { answer: false }))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.20:50051".parse().unwrap();
-
     println!("NostrCaesar listening on {}", addr);
 
+    let instance = MyNostrCaesar {
+        owner_pubkey: PublicKeyHex::try_from_str(EXAMPLE_PUBKEY)?,
+    };
+
     Server::builder()
-        .add_service(NostrCaesarServer::new(MyNostrCaesar { }))
+        .add_service(NostrCaesarServer::new(instance))
         .serve(addr)
         .await?;
 
